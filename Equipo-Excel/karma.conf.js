52fafcafe1a99b9aee851dfbf575cac724f7a59c
CarolinaSanchezP <92872811+CarolinaSanchezP@users.noreply.github.com>
2022-02-10 23:37:23 -0600

    Add files via upload

--- Equipo-Excel/karma.conf.js
+++ Equipo-Excel/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ReadExcel'),
+      dir: require('path').join(__dirname, './coverage/front-generate-reports'),
       subdir: '.',
       reporters: [
         { type: 'html' },
